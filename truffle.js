9d56fa25c5b0aecadbf9e722658bd28e087e6ba4
stobie <mark.stobie@gmail.com>
2018-08-02 21:30:47 +1200

    use ganache gui

--- truffle.js
+++ truffle.js
@@ -1,16 +1,16 @@
 module.exports = {
-  migrations_directory: "./migrations",
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    migrations_directory: "./migrations",
+    networks: {
+        development: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*" // Match any network id
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 500
+        }
     }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 500
-    }
-  } 
 };
