4d020fc638da2e8644d338807281177a5cb99fb7
stobie <mark.stobie@gmail.com>
2018-08-27 22:24:30 +1200

    added start of pagedraw files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
-import HomeContainer from './layouts/home/HomeContainer'
-
+//import HomeContainer from './layouts/home/HomeContainer'
+/* import the MainScreen component drawn above in Pagedraw */
+import MainScreen from './pagedraw/mainscreen'
 
 // Styles
 import './css/oswald.css'
@@ -9,14 +10,27 @@ import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
 
+var DaysEnum = Object.freeze({"intro":1, "send":2, "receive":3})
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Route exact path="/" component={HomeContainer}/>
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      selectedTab: DaysEnum.intro
+    };
   }
+
+  render() {
+    return <MainScreen selectedTab={this.state.selectedTab} setSelectedTab={(i) => this.setState({selectedTab: i})}/>;
+  }
+
+//  render() {
+//    return (
+//      <div className="App">
+//        <Route exact path="/" component={HomeContainer}/>
+//      </div>
+//    );
+//  }
 }
 
 export default App
