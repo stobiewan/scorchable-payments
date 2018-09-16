e32d8f1bc100fa2d628235c8cec8b0db5856e713
stobie <mark.stobie@gmail.com>
2018-09-16 12:15:46 +1200

    beginning of outgoing

--- src/layouts/drizzle_app/DrizzleApp.js
+++ src/layouts/drizzle_app/DrizzleApp.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 /* import the MainScreen component drawn above in Pagedraw */
 import MainScreen from '../../pagedraw/mainscreen'
 
-var DaysEnum = Object.freeze({"intro" : 1, "manageDai" : 2, "create" : 3, "ountgoing" : 4, "incoming" : 5})
+var DaysEnum = Object.freeze({"intro": 1, "manageDai": 2, "create": 3, "ountgoing": 4, "incoming": 5})
 
 class DrizzleApp extends Component {
-  constructor(props, context) {
-    super(props);
-    this.state = {
-      selectedTab: DaysEnum.intro,
-      accountsApprovedDai: "123"
-    };
-  }
+    constructor(props, context) {
+        super(props);
+        this.changeOutgoingPaymentIndex = this.changeOutgoingPaymentIndex.bind(this);
+        this.state = {
+            selectedTab: DaysEnum.intro,
+            selectedOutgoingIndex: 0
+        };
+    }
 
-  onApproveDai(quantity) {
-    this.setState({selectedTab: 3})
-  }
-
-  render() {
-    return <MainScreen selectedTab={this.state.selectedTab}
-                       setSelectedTab={(i) => this.setState({selectedTab: i})}
-                       accounts={this.props.accounts}
-                       onApproveDai={(i) => this.onApproveDai(i)}/>;
-  }
+    changeOutgoingPaymentIndex(change) {
+        console.log(change)
+        let newIndex = this.state.selectedOutgoingIndex + change;
+        if (newIndex < 0) {
+            newIndex = 0;
+        }
+        this.setState({selectedOutgoingIndex: newIndex});
+    }
+    render() {
+        return <MainScreen selectedTab={this.state.selectedTab}
+                           setSelectedTab={(i) => this.setState({selectedTab: i})}
+                           accounts={this.props.accounts}
+                           onChangeOutgoingIndex={this.changeOutgoingPaymentIndex}
+                           outgoingPaymentIndex={this.state.selectedOutgoingIndex}/>;
+    }
 }
 
 export default DrizzleApp
