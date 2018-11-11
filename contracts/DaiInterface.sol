aac8c03a2b4993fb23d007ae629b8a5653cdb055
stobie <mark.stobie@gmail.com>
2018-11-11 23:57:04 +1300

    updated Kovan Sai addresses

--- contracts/DaiInterface.sol
+++ contracts/DaiInterface.sol
@@ -9,7 +9,7 @@ contract DaiInterface {
 
 contract DaiTransferrer {
 
-    address daiAddress = 0x444254706E8F1FB62a6EC26A7FA2b942ef672495; // TODO Kovan, replace in drizzleOptions too
+    address daiAddress = 0xC4375B7De8af5a38a93548eb8453a498222C4fF2; // TODO Kovan, replace in drizzleOptions too. Mainnet is 0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359
     DaiInterface daiContract = DaiInterface(daiAddress);
 
     function transferDai(address _src, address _dst, uint _dai) internal {
