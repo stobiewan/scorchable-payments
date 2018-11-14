a2aee286afbc726c2d86b8641297ce7efb88d36b
stobie <mark.stobie@gmail.com>
2018-11-14 15:02:16 +1300

    removed router, used package.json from drizzlebox to fix production build

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route} from 'react-router'
 import {DrizzleProvider} from 'drizzle-react'
-
-// Layouts
-import App from './App'
-
-import LoadingContainer from './components/LoadingContainer'
 
 import {history, store} from './store'
 import drizzleOptions from './drizzleOptions'
+import DrizzleAppContainer from "./layouts/drizzle_app/DrizzleAppContainer";
+import LoadingContainer from './components/LoadingContainer'
 
 ReactDOM.render((
         <DrizzleProvider options={drizzleOptions} store={store}>
             <LoadingContainer>
-                <Router history={history} store={store}>
-                    <Route exact path="/" component={App}/>
-                </Router>
+                <DrizzleAppContainer/>
             </LoadingContainer>
         </DrizzleProvider>
     ),
